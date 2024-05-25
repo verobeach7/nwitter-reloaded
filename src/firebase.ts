55cc1af0f41e081d9facde5c161fbbad733e8b1c
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-05-25 17:35:39 +0900

    Nwitter: Firebase Project Setup

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfY4QVrVecS_5lFFwn2Qwy9gh2Y25O168",
+  authDomain: "nwitter-reloaded-2208f.firebaseapp.com",
+  projectId: "nwitter-reloaded-2208f",
+  storageBucket: "nwitter-reloaded-2208f.appspot.com",
+  messagingSenderId: "415966068330",
+  appId: "1:415966068330:web:5846b3f18e1bfdd675d413",
+};
+
+const app = initializeApp(firebaseConfig);
